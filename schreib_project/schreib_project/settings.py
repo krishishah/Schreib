f381769a62fc2d5b3738605efbc872562b93be44
root <root@test-14-1>
2016-05-27 18:32:30 +0100

    Changed settings in settings.py

--- schreib_project/schreib_project/settings.py
+++ schreib_project/schreib_project/settings.py
@@ -57,8 +57,6 @@ ROOT_URLCONF = 'schreib_project.urls'
 
 WSGI_APPLICATION = 'schreib_project.wsgi.application'
 
-heroku config:set DISABLE_COLLECTSTATIC=1
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
